1729f75ce0b698bd0fa7518d7ba52539cc731ad2
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-03-08 10:41:40 +0000

    updating  grib to xarray

--- climetlab_s2s_ai_competition/__init__.py
+++ climetlab_s2s_ai_competition/__init__.py
@@ -77,6 +77,11 @@ class S2sDataset(Dataset):
         request["extension"] = "grib"
         self.source = cml.load_source("url-pattern", PATTERN, request)
 
+    #        for s in self.source.sources:
+    #            print(s)
+    #            print(s.dataset)
+    #            s.dataset = self
+
     def _load_netcdf(self, *args, **kwargs):
         request = self._make_request(*args, **kwargs)
         request["format"] = "netcdf"
@@ -95,3 +100,57 @@ class S2sDataset(Dataset):
         urls = Pattern(ZARRPATTERN).substitute(request)
 
         self.source = cml.load_source("zarr-s3", urls)
+
+    def post_xarray_open_dataset_hook(self, ds):
+        # we may want also to add this too :
+        # import cf2cdm # this is from the package cfgrib
+        # ds = cf2cdm.translate_coords(ds, cf2cdm.CDS)
+        # or
+        # ds = cf2cdm.translate_coords(ds, cf2cdm.ECMWF)
+
+        if "number" in list(ds.coords):
+            ds = ds.rename({"number": "realization"})
+        if "time" in list(ds.coords):
+            ds = ds.rename({"time": "forecast_time"})
+        if "valid_time" in list(ds.coords):
+            ds = ds.rename({"valid_time": "time"})
+        if "heightAboveGround" in list(ds.coords):
+            # if we decide to keep it, rename it.
+            # ds = ds.rename({'heightAboveGround':'height_above_ground'})
+            ds = ds.squeeze("heightAboveGround")
+            ds = ds.drop("heightAboveGround")
+        if "surface" in list(ds.coords):
+            ds = ds.squeeze("surface")
+            ds = ds.drop("surface")
+        return ds
+
+    def cfgrib_options(self, time_convention="withstep"):
+        params = {}
+        if time_convention == "withstep":
+            time_dims = ["time", "step"]  # this is the default of engine='cfgrib'
+            chunk_sizes_in = {
+                "time": 1,
+                "latitude": None,
+                "longitude": None,
+                "number": 1,
+                "step": 1,
+            }
+
+        elif time_convention == "nostep":
+            time_dims = ["time", "valid_time"]
+            chunk_sizes_in = {
+                "time": 1,
+                "latitude": None,
+                "longitude": None,
+                "number": 1,
+                "valid_time": 1,
+            }
+
+        else:
+            raise Exception()
+
+        # params['engine'] = 'cfgrib'
+        params["chunks"] = chunk_sizes_in
+        params["backend_kwargs"] = dict(squeeze=False, time_dims=time_dims)
+
+        return params
