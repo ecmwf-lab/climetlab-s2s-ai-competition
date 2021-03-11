7b41da37463f01411580ebd5e0f459a3017b6b6f
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-03-11 16:26:04 +0000

    0.2.4

--- climetlab_s2s_ai_competition/__init__.py
+++ climetlab_s2s_ai_competition/__init__.py
@@ -8,7 +8,7 @@
 
 
 # note this version number has nothing to do with the version number of the dataset
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 import climetlab as cml
 from climetlab import Dataset
